0031ae73efe82d3476e046fc916b2777b7ab9960
FantinPro <raimbault.fantin.pro@gmail.com>
2022-07-27 19:35:04 +0200

    fix api url config

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,4 +1,4 @@
 const config = {
-    apiUrl: import.meta.env.VITE_BASE_API_URL || 'http://localhost:9000/api/v1',
+    apiUrl: (import.meta.env.VITE_BASE_API_URL || 'http://localhost:9000/api/v1') + '/api/v1',
 };
 export default config;
